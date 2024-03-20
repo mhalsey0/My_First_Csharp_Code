13225289f9ab26bdbc40231eb190cecce2498b17
Mike Halsey <mhalsey0@gmail.com>
2024-03-20 15:53:10 -0400

    first commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace workspace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is the name of your venue?");
+            string venue = args[0];
+            Console.WriteLine(Announce(venue));
+        }
+        static string Announce(string venue)
+        {
+            return venue + " will have bands performing tonight.";
+        }
+    }
+}
+
